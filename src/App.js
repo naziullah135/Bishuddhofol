2e9fb5b51cd2276acf2de8375737bf75142ac7f3
Alamin Howlader <alaminhowladerofficial@gmail.com>
2021-05-15 03:04:17 +0600

    added navigation and banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
-
+import Navigation from './components/Navigation/Navigation';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Banner from './components/Banner/Banner';
 const App = () => {
   return (
-    <div>
-      hello
-    </div>
+    <Router>
+      <Navigation />
+      <Banner />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Switch>
+
+      </Switch>
+    </Router>
   );
 };
 
