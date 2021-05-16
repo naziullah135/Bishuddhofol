df6408096d1003e95ca17acdda3aca4c640e0042
Naziullah Shawn <naziullahshawn135@gmail.com>
2021-05-17 01:43:53 +0600

    added about section

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Navigation from "./components/Navigation/Navigation";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Banner from "./components/Banner/Banner";
 import Shop from "./components/Shop/Shop";
+import About from "./components/About/About";
 const App = () => {
   return (
     <Router>
       <Navigation />
       <Banner />
+      <About />
       <Shop />
       <br />
       <br />
---
+++ src/components/About/About.js
@@ -0,0 +1,30 @@
+import { Container, Grid } from "@material-ui/core";
+import img from "../../images/static.jpg";
+import React from "react";
+
+const About = () => {
+  return (
+    <section>
+      <Container>
+        <Grid container spacing={3}>
+          <Grid item lg={8} md={8} sm={6} xs={12}>
+            <iframe
+              width="100%"
+              height="100%"
+              src="https://www.youtube.com/embed/wf3WqjSBqXM"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </Grid>
+          <Grid item lg={4} md={4} sm={6} xs={12}>
+            <img src={img} style={{ width: "100%", height: "400px" }} alt="" />
+          </Grid>
+        </Grid>
+      </Container>
+    </section>
+  );
+};
+
+export default About;
